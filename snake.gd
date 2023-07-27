45c1a5adb781f0b265019d9bbdfc2720d3c8ee8a
JanBat <janbatelka@gmail.com>
2023-07-27 13:21:55 +0200

    basic segments follow each other now

--- snake.gd
+++ snake.gd
@@ -1,18 +1,31 @@
 extends Sprite2D
 
-var speed = 200
+var speed = 164
 var direction = Vector2.ZERO
 
 var new_direction: Vector2
 
+@export var next_segment : Sprite2D
+
 func _ready():
 	direction.x = speed
 	new_direction = direction
+	var next = next_segment
+	
+	# recursively initialize all lower segments
+	while next:
+		next.direction = direction
+		next.speed = speed
+		next = next.next_segment
 
 func _on_move_timer_timeout():
-	direction = new_direction
-
-
+	var next = self
+	while next:
+		if next.next_segment:
+			next.next_segment.new_direction = next.direction
+		next.direction = next.new_direction
+		next = next.next_segment
+	
 func _process(delta):
 	if Input.is_action_just_pressed("steer_right"):
 		new_direction = direction.rotated(PI / 2)
@@ -22,8 +35,5 @@ func _process(delta):
 		new_direction = direction.rotated(-PI / 2)
 	if Input.is_action_just_pressed("keep_going_straight"):
 		new_direction = direction
-
 	# apply movement
-	print(direction)
-	print(new_direction)
-	position += direction *  delta
+	position += direction * delta
