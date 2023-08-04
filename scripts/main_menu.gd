c86c1af15bb5e98b272c32db797826903fa722c7
JanBat <janbatelka@gmail.com>
2023-08-04 12:14:38 +0200

    cleanup!

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,7 +8,7 @@ func _process(delta):
 #--------------------------------------------------------
 #  BUTTON FUCTIONS ON MAIN MENU
 func _on_start_pressed():
-	var start_scene = preload("res://level.tscn").instantiate() #Not in main function because want new game to start only when button pressed
+	var start_scene = preload("res://scenes/level.tscn").instantiate() #Not in main function because want new game to start only when button pressed
 	start_scene.game_over.connect(on_game_over)  #Allows game over function to show handler for creating a new scene
 	print("START GAME")
 	get_tree().root.add_child(start_scene)
