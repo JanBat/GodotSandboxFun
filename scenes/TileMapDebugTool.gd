425b84e146f2c8df70d3216578f5b421b60a9224
JanBat <janbatelka@gmail.com>
2023-08-09 13:29:53 +0200

    optional tilelabels for debugging purposes

---
+++ scenes/TileMapDebugTool.gd
@@ -0,0 +1,21 @@
+@tool # doesn't necessarily do anything (yet?)
+extends TileMap
+
+@export
+var debug = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if debug:
+		for layer in range(5):
+			for cell in get_used_cells(layer):
+				var tile_size = tile_set.tile_size
+				var new_label = $Label.duplicate()
+				new_label.text = str(cell)
+				new_label.position = map_to_local(Vector2(cell)) - tile_size * 0.25
+				new_label.show()
+				add_child(new_label)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
