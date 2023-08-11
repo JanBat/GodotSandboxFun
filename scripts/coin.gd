2f85739c31bfd04f88e6e6ab626f3144b62f4fac
JanBat <janbatelka@gmail.com>
2023-08-11 13:16:14 +0200

    coin spawning now ignores snake segments

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal no_more_space_for_coins()
 
 @export var tile_map: TileMap
 @export var grid_location: Vector2
@@ -20,17 +21,20 @@ func get_gobbled_up():
 		# layer 1 contains all the trees:
 		func(cell): return not tile_map.get_cell_tile_data(1, cell)
 	)
-	# remove coins
-	var all_coins = get_tree().get_nodes_in_group("coin")
-	var all_coin_positions = all_coins.map(
-		func(coin): return tile_map.local_to_map(coin.position)
-	)
+	# remove coins and snakes
+	for group in ["coin", "snake"]:
+		var elements = get_tree().get_nodes_in_group(group)
+		var all_element_positions = elements.map(
+			func(element): return tile_map.local_to_map(element.position)
+		)
+		
+		position_candidates = position_candidates.filter(
+			func(cell): return cell not in all_element_positions
+		)
 	
-	position_candidates = position_candidates.filter(
-		func(cell): return cell not in all_coin_positions
-	)
-	
-	# remove snakesegments
-	
-	position = tile_map.map_to_local(position_candidates[randi_range(0, len(position_candidates))])
+	if position_candidates:
+		position = tile_map.map_to_local(position_candidates[randi_range(0, len(position_candidates))])
+	else:
+		no_more_space_for_coins.emit()
+		queue_free()
 
