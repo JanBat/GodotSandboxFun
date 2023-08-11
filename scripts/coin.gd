dcfd019509e0bed8f1540b4013ad0b7dedfb8806
JanBat <janbatelka@gmail.com>
2023-08-11 13:01:37 +0200

    coins now ostensibly don't respawn on top of other coins

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -17,9 +17,19 @@ func get_gobbled_up():
 	var position_candidates = tile_map.get_used_cells(0)
 	# remove trees
 	position_candidates = position_candidates.filter(
+		# layer 1 contains all the trees:
 		func(cell): return not tile_map.get_cell_tile_data(1, cell)
 	)
 	# remove coins
+	var all_coins = get_tree().get_nodes_in_group("coin")
+	var all_coin_positions = all_coins.map(
+		func(coin): return tile_map.local_to_map(coin.position)
+	)
+	
+	position_candidates = position_candidates.filter(
+		func(cell): return cell not in all_coin_positions
+	)
+	
 	# remove snakesegments
 	
 	position = tile_map.map_to_local(position_candidates[randi_range(0, len(position_candidates))])
